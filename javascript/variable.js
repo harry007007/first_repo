d75bf1c7690f2df836c69a70b67a6d95207c4750
Harish <haridaspawar03@gmail.com>
2023-12-04 14:27:09 +0530

    my work updated

---
+++ javascript/variable.js
@@ -0,0 +1,15 @@
+
+//let myFirstName = 'harish';
+
+//let myFirstName
+//myFirstName = 'harish';
+
+//var sName = 'pawar';
+
+//var sName
+//sName = 'pawar';
+//sName = 'patil'
+
+const cityName = 'mumbai';
+
+console.log ('I LOVE',cityName);
